4c158d026b68b1510ab0decc8defdf6caf0182cb
vitorslongo <v.v.slongo@gmail.com>
2025-04-10 18:19:39 -0300

    add  implementation of structured mesh generation using gmsh

---
+++ Parte 2/malha_estruturada.py
@@ -0,0 +1,59 @@
+import gmsh
+occ = gmsh.model.occ
+mesh = gmsh.model.mesh
+
+gmsh.initialize()
+
+b = 0.2
+h = 0.2
+t = 0.05
+
+occ.addRectangle(-b/2, -h/2, 0, t, t)
+occ.addRectangle(b/2 - t, -h/2, 0, t, t)
+occ.addRectangle(-b/2, h/2 - t, 0, t, t)
+occ.addRectangle(b/2 - t, h/2 - t, 0, t, t)
+
+points = (2, 5, 3, 8, 7, 14, 8, 13, 13, 10, 16, 11, 10, 3, 9, 4)
+
+for i in range(0, len(points), 2):
+    occ.addLine(points[i], points[i+1])
+
+lines = [
+    [17, 8, 18, 2],
+    [7, 19, 13, 20],
+    [21, 16, 22, 10],
+    [3, 23, 9, 24],
+    ]
+wires = []
+for rectangle in lines:
+    wires.append(occ.addCurveLoop(rectangle))
+
+surfaces = []
+for wire in wires:
+    surfaces.append(occ.addSurfaceFilling(wire))
+
+occ.synchronize()
+occ.extrude(gmsh.model.getEntities(2), 0, 0, 1, recombine=True)
+
+occ.synchronize()
+
+volume_tags = []
+for _, voltag in gmsh.model.getEntities(3):
+    volume_tags.append(voltag)
+
+surface_tags = []
+for _, surface_tag in gmsh.model.getEntities(2):
+    surface_tags.append(surface_tag)
+
+for surface in surface_tags:
+    mesh.setTransfiniteSurface(surface)
+
+for volume in volume_tags:
+    mesh.setTransfiniteVolume(volume)
+
+gmsh.option.setNumber("Mesh.MeshSizeFactor", .3)
+gmsh.option.setNumber("Mesh.RecombineAll", 1)
+
+mesh.generate(3)
+gmsh.fltk.run()
+gmsh.finalize()
