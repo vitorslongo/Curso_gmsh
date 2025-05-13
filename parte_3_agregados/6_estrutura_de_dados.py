07c844a1384995025718fe7613da88e409f2f95a
vitorslongo <v.v.slongo@gmail.com>
2025-05-13 16:26:16 -0300

    oie

--- parte_3_agregados/6_estrutura_de_dados.py
+++ parte_3_agregados/6_estrutura_de_dados.py
@@ -1,15 +1,21 @@
 """
 Aqui nós vamos pegar as informações que o Gmsh nos sobre a malha dá de uma maneira meio ruim e vamos organizar os dados de forma com a qual possamos
 trabalhar com eles e implementar nos nossos solvers.
+
+A matriz representa quais nós formam cada elemento. Ela é usada para:
+    - construir malhas em outros programas (ex: ParaView, solver próprio)
+    - entender a topologia da malha
+    - aplicar condições de contorno com base em elementos
 """
 import gmsh
 import numpy as np
+from pprint import pprint
 gmsh.initialize()
 occ = gmsh.model.occ
 mesh = gmsh.model.mesh
 gmsh.option.setNumber("General.Verbosity", 0)
 
-# Vamos só criar um retângulo e malhar ele normalmente.
+# Vamos só criar um quadrado e malhar ele normalmente.
 occ.addRectangle(0, 0, 0, 1, 1)
 occ.synchronize()
 
@@ -19,21 +25,21 @@ mesh.generate(3)
 # Aqui pegamos as informações de coordenadas nodais, de maneira crua. O Gmsh entrega ela em uma lista gigante, tudo concatenado.
 node_indexes, coords, _ = mesh.getNodes(2, -1, True)
 
-# O que fazemos aqui é basicamente darmos o nosso próprio nome para os nós, basicamente um apelido, e organizamos uma matriz de forma que:
+# O que fazemos aqui é basicamente dar o nosso próprio nome para os nós, basicamente um apelido, e organizamos uma matriz de forma que:
 # Sendo "n" o número de nós da malha, criamos uma matriz n x 4, onde a primeira coluna é o apelido do nó e as outras 3 são as coordenadas dele.
-n_nodes = len(node_indexes)
-n_indexes = np.arange(n_nodes, dtype=int)
-section_nodal_coordinates = np.zeros((n_nodes, 4))
-section_nodal_coordinates[n_indexes, 1:] = coords.reshape(-1, 3)
-section_nodal_coordinates[n_indexes, :1] = node_indexes.reshape(-1, 1) - 1
+n_nodes = len(node_indexes) # o numero de nós é a quantidade de indexes de nós
+n_indexes = np.arange(n_nodes, dtype=int) # crio a lista com os indexes pra facilitar a atribuição depois
+section_nodal_coordinates = np.zeros((n_nodes, 4)) # inicializo a matriz de coordenadas nodais
+section_nodal_coordinates[n_indexes, 1:] = coords.reshape(-1, 3) # atribuo as coordenadas do "coords" pra cada nó
+section_nodal_coordinates[n_indexes, :1] = node_indexes.reshape(-1, 1) - 1 # depois eu coloco os indexes (apelidos) na matriz 
 
 # A outra matriz que precisamos é a de conectividade nodal, onde nela podemos tirar todas as informações de quais nós estão conectados a quais.
 # Ela é feita da seguinte forma: Considerando que nossa malha tenha apenas triangulos (já que é uma malha 2D), cada elemento vai ter m = 3 nós.
-# Então, 
+# Então, pego as infomações de tipo de elementos que temos na nossa malha, os indexes de cada um deles e os nós que fazem parte de cada elemento
 element_types, element_indexes, element_nodes = gmsh.model.mesh.getElements(2, -1)
 
 section_connectivities = dict()
-
+# Para cada elemento:
 for i in range(len(element_nodes)):
     element_name, _, _, nodes_per_element, _, _ = gmsh.model.mesh.getElementProperties(element_types[i])
     
@@ -51,7 +57,8 @@ for i in range(len(element_nodes)):
     section_connectivities[element_name] = section_connectivity
 
 
-
+# pprint(section_nodal_coordinates)
+pprint(section_connectivities)
 
 # print("node_tags:", node_tags)
 
@@ -61,5 +68,5 @@ for i in range(len(element_nodes)):
 
 
 
-# gmsh.fltk.run()
+gmsh.fltk.run()
 gmsh.finalize()
--- parte_3_agregados/8_qualidade_malha.py
+++ parte_3_agregados/8_qualidade_malha.py
@@ -4,7 +4,6 @@ gmsh.option.setNumber("General.Terminal", 0)
 
 occ = gmsh.model.occ
 mesh = gmsh.model.mesh
-# gmsh.open("C:\\Users\\vvslo\\Desktop\\fluido_silenciador.STEP")
 
 # Vamos gerar uma geometria que com certeza vai gerar elementos distorcidos para visualizarmos os parametros e seus valores
 main = occ.addRectangle(0, 0, 0, 1, 2)
@@ -18,9 +17,13 @@ occ.synchronize()
 
 gmsh.option.setNumber("Mesh.Algorithm", 7)
 gmsh.option.setNumber("Mesh.CharacteristicLengthFactor", 0.5)
-mesh.generate(3)
 
-print(mesh.get_elements(3, 1))
+
+# gmsh.option.setNumber("Mesh.Smoothing", 100)
+mesh.generate(3)
+# gmsh.option.setNumber("Mesh.Optimize", 3)
+
+# print(mesh.get_elements(3, 1))
 # parameter = "gamma" # razao do raio da esfera inscrita e circunscrita --> se for muito grande é ruim
 # parameter = "volume" # volume do elemento
 # parameter = "minSJ" 
@@ -37,16 +40,16 @@ o mais utilizado na indústria, varia entre -1 (invertido, péssimo) e 1 (ótimo
 Valor aceitável: > 0.2 geralmente.
 
 minSIGE: Minimum Scaled Inverse Gradient Error
-Mede o erro na interpolação do gradiente dentro do elemento.
+Mede o erro na interpolação do gradiente dentro do elemento.s
 Quanto menor, pior a capacidade de resolver gradientes físicos (temperatura, velocidade, etc).
-Valor entre 0() e 1. 
+Valor entre 0 (ruim) e 1 (ótimo). 
 Bom pra problemas de transferência de calor e CFD onde o gradiente importa muito.
 
 minSICN: minimum scaled inverse condition number (determinante do jacobiano divido pela norma de Frobenius do jacobiano)
 Parâmetro de controle de qualidade de malha mais completo, carrega 3 informações: 
 determinante do jacobiano --> volume do elemento, evita elementos colapsados
 sinal do determinante do jacobiano --> orientação do sistema de coordendas locais do elemento, evita inversoes
-norma de Frobenius (nornalizada) --> soma do tamanho dos vetores que compoem o sist. de corrd. locais, deteca esticamento do elemento
+norma de Frobenius (nornalizada) --> soma do tamanho dos vetores que compoem o sistema de coordenadas locais, deteca esticamento do elemento
 """
 
 for element in mesh.get_elements(3, 1)[1]:
@@ -56,7 +59,6 @@ for element in mesh.get_elements(3, 1)[1]:
 print(parameter, "min:", min)
 print(parameter,"max:", max)
 
-# print(qualities)
 gmsh.fltk.run()
 gmsh.finalize()
 
